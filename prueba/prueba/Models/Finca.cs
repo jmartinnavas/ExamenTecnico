92b75e082545108b726693d4d0831337611fa744
jmartinnavas <juan.1701212536@ucaldas.edu.co>
2019-05-30 01:25:38 -0500

    entidades

---
+++ prueba/prueba/Models/Finca.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace prueba.Models
+{
+    public class Finca
+    {
+        public int FincaId { get; set; }
+
+        public string nombre { get; set; }
+
+        public string vereda { get; set; }
+
+        public string municipio { get; set; }
+
+        public string departamento { get; set; }
+
+    }
+}
---
+++ prueba/prueba/Models/Trabajador.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace prueba.Models
+{
+    public class Trabajador
+    {
+        public int TrabajadorId { get; set; }
+
+        public string nombre { get; set; }
+
+        public string apellido { get; set; }
+
+        public string contacto { get; set; }
+
+        public string departamento { get; set; }
+    }
+}
